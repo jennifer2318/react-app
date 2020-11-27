3851bc4448d774e27b850f5743f5fcb8ffff8659
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2020-11-27 14:55:15 +0300

    reducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import loginReducer from "./loginReducer";
+import messageBarReducer from "./messageBarReducer";
 
 export default combineReducers({
-
+    login: loginReducer,
+    messageBar: messageBarReducer,
 });
